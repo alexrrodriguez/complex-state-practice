7f259ea89b1d601064c0af3d1b235f8cc465bd35
alex <arodriguez202020@gmail.com>
2021-12-13 16:49:08 -0600

    import useState, add onChange event listener to fname and lname inputs, use useState to create first and last name state variable and functions,  create functions and pass in event to state function in order to change value first and last name in heading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFName(event) {
+    console.log(event.target.value);
+    setFirstName(event.target.value);
+  }
+
+  function handleLName(event) {
+    console.log(event.target.value);
+    setLastName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleFName} name="fName" placeholder="First Name" />
+        <input onChange={handleLName} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
